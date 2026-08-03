dc571f68bc93b36daa4f4290496114d7de90dad9
Oluwashola Olumide <oaolumide1@gmail.com>
2026-08-03 14:56:16 -0700

    Format Terraform files

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -8,14 +8,14 @@ module "alb_sg" {
 
   vpc_id = var.vpc_id
 
- ingress_rules = [
-  "http-80-tcp",
-  "https-443-tcp"
-]
+  ingress_rules = [
+    "http-80-tcp",
+    "https-443-tcp"
+  ]
 
-ingress_cidr_blocks = [
-  "0.0.0.0/0"
-]
+  ingress_cidr_blocks = [
+    "0.0.0.0/0"
+  ]
 
   egress_rules = [
     "all-all"
